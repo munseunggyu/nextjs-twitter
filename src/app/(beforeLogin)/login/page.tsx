fa0077114605d78b2c3050034e4c00b7e8708a2b
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-12-06 23:30:22 +0900

    로그인 버튼 클릭시 /i/flow/login 페이지로 replace

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from 'next/navigation';
+'use client';
+import { useRouter } from 'next/navigation';
+import Main from '../_component/Main';
+import { useEffect } from 'react';
 
 export default function Login() {
-  redirect('/i/flow/login');
+  // redirect('/i/flow/login'); 서버에서 이동
+  const router = useRouter(); // 클라이언트 이동
+
+  useEffect(() => {
+    router.replace('/i/flow/login');
+  }, []);
+  return <Main />;
 }
