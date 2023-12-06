9e798553654eeafc73628c68dee01e7395ba1640
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-12-06 22:04:47 +0900

    before login 페이지 추가

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,12 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import zLogo from '../../../public/zlogo.png';
+import styles from '@/app/(beforeLogin)/_component/main.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        gd
-        <Image src="/vercel.svg" alt="logo" width={60} height={60} />
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <Image src={zLogo} alt="logo" />
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요.</h2>
+        <Link href={'/i/flow/signup'}>계정 만들기</Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href={'/login'} className="">
+          로그인
+        </Link>
       </div>
     </div>
   );
