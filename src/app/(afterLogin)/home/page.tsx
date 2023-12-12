a4f3980804a76d32a978fc1c681f7f3809dace1c
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-12-12 22:23:37 +0900

    main home 추가

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import style from './home.module.css';
+import Tab from '@/app/(afterLogin)/home/_component/Tab';
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
+import Post from '@/app/(afterLogin)/_component/Post';
+import TabProvider from './_component/TabProvider';
 
-export default function page() {
-  return <div>home page</div>;
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 }
