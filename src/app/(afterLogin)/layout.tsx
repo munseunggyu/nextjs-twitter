75c49f1ff7ceabf88943c2e66e0b0b9c4511e4db
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-12-17 20:55:23 +0900

    react-query 설치 및 기본 세팅

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -9,6 +9,7 @@ import TrendSection from '@/app/(afterLogin)/_component/TrendSection';
 import FollowRecommend from '@/app/(afterLogin)/_component/FollowRecommend';
 import RightSearchZone from './_component/RightSearchZone';
 import { auth } from '@/auth';
+import RQProvider from './_component/RQProvider';
 
 export default async function AfterLoginLayout({
   children,
@@ -20,46 +21,48 @@ export default async function AfterLoginLayout({
   const session = await auth();
   return (
     <div className={style.container}>
-      <header className={style.leftSectionWrapper}>
-        <section className={style.leftSection}>
-          <div className={style.leftSectionFixed}>
-            <Link className={style.logo} href="/home">
-              <div className={style.logoPill}>
-                <Image src={ZLogo} alt="z.com로고" width={40} height={40} />
-              </div>
-            </Link>
-            {session?.user && (
-              <>
-                <nav>
-                  <ul>
-                    <NavMenu />
-                  </ul>
-                  <Link href="/compose/tweet" className={style.postButton}>
-                    게시하기
-                  </Link>
-                </nav>
-                <LogoutButton />
-              </>
-            )}
-          </div>
-        </section>
-      </header>
-      <div className={style.rightSectionWrapper}>
-        <div className={style.rightSectionInner}>
-          <main className={style.main}>{children}</main>
-          <section className={style.rightSection}>
-            <RightSearchZone />
-            <TrendSection />
-            <div className={style.followRecommend}>
-              <h3>팔로우 추천</h3>
-              <FollowRecommend />
-              <FollowRecommend />
-              <FollowRecommend />
+      <RQProvider>
+        <header className={style.leftSectionWrapper}>
+          <section className={style.leftSection}>
+            <div className={style.leftSectionFixed}>
+              <Link className={style.logo} href="/home">
+                <div className={style.logoPill}>
+                  <Image src={ZLogo} alt="z.com로고" width={40} height={40} />
+                </div>
+              </Link>
+              {session?.user && (
+                <>
+                  <nav>
+                    <ul>
+                      <NavMenu />
+                    </ul>
+                    <Link href="/compose/tweet" className={style.postButton}>
+                      게시하기
+                    </Link>
+                  </nav>
+                  <LogoutButton />
+                </>
+              )}
             </div>
           </section>
+        </header>
+        <div className={style.rightSectionWrapper}>
+          <div className={style.rightSectionInner}>
+            <main className={style.main}>{children}</main>
+            <section className={style.rightSection}>
+              <RightSearchZone />
+              <TrendSection />
+              <div className={style.followRecommend}>
+                <h3>팔로우 추천</h3>
+                <FollowRecommend />
+                <FollowRecommend />
+                <FollowRecommend />
+              </div>
+            </section>
+          </div>
         </div>
-      </div>
-      {modal}
+        {modal}
+      </RQProvider>
     </div>
   );
 }
