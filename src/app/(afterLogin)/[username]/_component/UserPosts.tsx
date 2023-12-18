91e907ff61f5a879095421c71fd6d21e8dafc9cc
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-12-19 00:05:03 +0900

    사용자 프로필 > 게시글 페이지 완성, 이미지 상세페이지 완성

--- src/app/(afterLogin)/[username]/_component/UserPosts.tsx
+++ src/app/(afterLogin)/[username]/_component/UserPosts.tsx
@@ -1,27 +1,37 @@
-"use client";
+'use client';
 
-import {useQuery, useQueryClient} from "@tanstack/react-query";
-import {getUserPosts} from "../_lib/getUserPosts";
-import Post from "@/app/(afterLogin)/_component/Post";
-import {Post as IPost} from "@/model/Post";
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getUserPosts } from '../_lib/getUserPosts';
+import Post from '@/app/(afterLogin)/_component/Post';
+import { Post as IPost } from '@/model/Post';
+import { useEffect, useState } from 'react';
 
 type Props = {
   username: string;
-}
+};
 export default function UserPosts({ username }: Props) {
-  const { data } = useQuery<IPost[], Object, IPost[], [_1: string, _2: string, _3: string]>({
+  const { data } = useQuery<
+    IPost[],
+    Object,
+    IPost[],
+    [_1: string, _2: string, _3: string]
+  >({
     queryKey: ['posts', 'users', username],
     queryFn: getUserPosts,
     staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
-    gcTime: 300 * 1000,
+    gcTime: 300 * 1000
   });
   const queryClient = useQueryClient();
   const user = queryClient.getQueryData(['users', username]);
   console.log('user', user);
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
   if (user) {
-    return data?.map((post) => (
-      <Post key={post.postId} post={post} />
-    ))
+    return data?.map(post => <Post key={post.postId} post={post} />);
   }
   return null;
 }
