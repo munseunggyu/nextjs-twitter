61e0ad50df228b9ec8963c537194c8639de1670b
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-12-31 18:39:47 +0900

    api 주소 변경 적용

--- src/app/(afterLogin)/_lib/getFollowRecommends.ts
+++ src/app/(afterLogin)/_lib/getFollowRecommends.ts
@@ -1,17 +1,20 @@
 export async function getFollowRecommends() {
-  const res = await fetch(`http://localhost:9090/api/followRecommends`, {
-    next: {
-      tags: ['users', 'followRecommends'],
-    },
-    cache: 'no-store',
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/followRecommends`,
+    {
+      next: {
+        tags: ['users', 'followRecommends']
+      },
+      cache: 'no-store'
+    }
+  );
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
-  return res.json()
+  return res.json();
 }
