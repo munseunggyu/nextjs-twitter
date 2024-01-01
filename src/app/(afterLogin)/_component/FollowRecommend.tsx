5d0ac098db1eb021bf8863b66faa0ebf79b4e36b
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-01-02 00:11:22 +0900

    팔로우, 언팔로우 기능 추가

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -2,13 +2,121 @@
 
 import { User } from '@/model/User';
 import style from './followRecommend.module.css';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { constant } from '@/app/constant';
+import { useSession } from 'next-auth/react';
 
 type IProps = {
   user: User;
 };
 
 export default function FollowRecommend({ user }: IProps) {
-  const onFollow = () => {};
+  const { data: session } = useSession();
+  const queryClient = useQueryClient();
+  const followed = !!user.Followers.find(v => {
+    return v.id === session?.user?.email;
+  });
+  const follow = useMutation({
+    mutationFn: () => {
+      return fetch(constant.apiUrl + `/api/users/${user.id}/follow`, {
+        method: 'post',
+        credentials: 'include'
+      });
+    },
+    onMutate() {
+      const oldData: User[] | undefined = queryClient.getQueryData([
+        'users',
+        'followRecommends'
+      ]);
+      if (oldData) {
+        const shallow = [...oldData];
+        let resultData;
+        if (followed) {
+          resultData = shallow.map(v => {
+            if (user.id === v.id) {
+              return {
+                ...v,
+                Followers: v.Followers.filter(
+                  x => x.id !== session?.user?.email
+                )
+              };
+            }
+            return v;
+          });
+        } else {
+          resultData = shallow.map(v => {
+            if (user.id === v.id) {
+              return {
+                ...v,
+                Followers: [...v.Followers, { id: session?.user?.email }]
+              };
+            }
+            return v;
+          });
+        }
+        console.log('resultData', resultData);
+        queryClient.setQueryData(['users', 'followRecommends'], resultData);
+      }
+
+      console.log('팔로우 완', oldData);
+    },
+    onError() {},
+    onSettled() {}
+  });
+  const unfollow = useMutation({
+    mutationFn: () => {
+      return fetch(constant.apiUrl + `/api/users/${user.id}/follow`, {
+        method: 'delete',
+        credentials: 'include'
+      });
+    },
+    onMutate() {
+      const oldData: User[] | undefined = queryClient.getQueryData([
+        'users',
+        'followRecommends'
+      ]);
+      if (oldData) {
+        const shallow = [...oldData];
+        let resultData;
+        if (followed) {
+          resultData = shallow.map(v => {
+            if (user.id === v.id) {
+              return {
+                ...v,
+                Followers: v.Followers.filter(
+                  x => x.id !== session?.user?.email
+                )
+              };
+            }
+            return v;
+          });
+        } else {
+          resultData = shallow.map(v => {
+            if (user.id === v.id) {
+              return {
+                ...v,
+                Followers: [...v.Followers, { id: session?.user?.email }]
+              };
+            }
+            return v;
+          });
+        }
+        queryClient.setQueryData(['users', 'followRecommends'], resultData);
+      }
+
+      console.log('팔로우 완', oldData);
+    },
+    onError() {},
+    onSettled() {}
+  });
+
+  const onFollow = () => {
+    if (followed) {
+      unfollow.mutate();
+    } else {
+      follow.mutate();
+    }
+  };
 
   return (
     <div className={style.container}>
@@ -22,7 +130,9 @@ export default function FollowRecommend({ user }: IProps) {
         <div className={style.count}>@{user.id}</div>
       </div>
       <div className={style.followButtonSection}>
-        <button onClick={onFollow}>팔로우</button>
+        {session?.user?.email !== user.id && (
+          <button onClick={onFollow}>{followed ? '팔로잉' : '팔로우'}</button>
+        )}
       </div>
     </div>
   );
