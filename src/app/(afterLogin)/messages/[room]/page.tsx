353d434f9cc2d4fb14e714a042015762bab24344
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-01-06 22:31:14 +0900

    메시지 상세페이지 메시지 form, 상대방 정보 추가

--- src/app/(afterLogin)/messages/[room]/page.tsx
+++ src/app/(afterLogin)/messages/[room]/page.tsx
@@ -1,59 +1,94 @@
-import {faker} from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 import style from './chatRoom.module.css';
-import Link from "next/link";
-import BackButton from "@/app/(afterLogin)/_component/BackButton";
-import cx from 'classnames'
-import relativeTime from "dayjs/plugin/relativeTime";
+import Link from 'next/link';
+import BackButton from '@/app/(afterLogin)/_component/BackButton';
+import cx from 'classnames';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
+import MessageForm from '../_component/MessageForm';
+import { auth } from '@/auth';
+import { QueryClient } from '@tanstack/react-query';
+import { getUserServer } from '../../[username]/_lib/getUserServer';
+import { UserInfo } from '../_component/UserInfo';
 
 dayjs.locale('ko');
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
 
-export default function ChatRoom() {
-  const user = {
-    id: 'hero',
-    nickname: '영웅',
-    image: faker.image.avatar(),
-  }
+type Props = {
+  params: { room: string };
+};
+
+export default async function ChatRoom({ params }: Props) {
+  // const user = {
+  //   id: 'hero',
+  //   nickname: '영웅',
+  //   image: faker.image.avatar()
+  // };
   const messages = [
-    {messageId: 1, roomId: 123, id: 'zerohch0',  content: '안녕하세요.', createdAt: new Date()},
-    {messageId: 2, roomId: 123, id: 'hero', content: '안녕히가세요.', createdAt: new Date()},
-  ]
+    {
+      messageId: 1,
+      roomId: 123,
+      id: 'zerohch0',
+      content: '안녕하세요.',
+      createdAt: new Date()
+    },
+    {
+      messageId: 2,
+      roomId: 123,
+      id: 'hero',
+      content: '안녕히가세요.',
+      createdAt: new Date()
+    }
+  ];
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['users', params.room],
+    queryFn: getUserServer
+  });
+
+  const session = await auth();
+  const myId = session?.user?.email;
+
+  const ids = params.room.split('-');
+  const userId = ids.find(v => v !== myId);
+  if (!userId) return null;
 
   return (
     <main className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <div><h2>{user.nickname}</h2></div>
-      </div>
-      <Link href={user.nickname} className={style.userInfo}>
-        <img src={user.image} alt={user.id} />
-        <div><b>{user.nickname}</b></div>
-        <div>@{user.id}</div>
-      </Link>
+      <UserInfo id={userId} />
       <div className={style.list}>
-        {messages.map((m) => {
-          if (m.id === 'zerohch0') { // 내 메시지면
+        {messages.map(m => {
+          if (m.id === 'zerohch0') {
+            // 내 메시지면
             return (
               <div
                 key={m.messageId}
-                className={cx(style.message, style.myMessage)}>
+                className={cx(style.message, style.myMessage)}
+              >
                 <div className={style.content}>{m.content}</div>
-                <div className={style.date}>{dayjs(m.createdAt).format('YYYY년 MM월 DD일 A HH시 mm분')}</div>
+                <div className={style.date}>
+                  {dayjs(m.createdAt).format('YYYY년 MM월 DD일 A HH시 mm분')}
+                </div>
               </div>
             );
           }
           return (
             <div
               key={m.messageId}
-              className={cx(style.message, style.yourMessage)}>
+              className={cx(style.message, style.yourMessage)}
+            >
               <div className={style.content}>{m.content}</div>
-              <div className={style.date}>{dayjs(m.createdAt).format('YYYY년 MM월 DD일 A HH시 mm분')}</div>
+              <div className={style.date}>
+                {dayjs(m.createdAt).format('YYYY년 MM월 DD일 A HH시 mm분')}
+              </div>
             </div>
           );
         })}
       </div>
+      <MessageForm id={userId} />
     </main>
-  )
+  );
 }
--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,18 +1,22 @@
+import { auth } from '@/auth';
+import { getRooms } from './_lib/getRooms';
 import style from './message.module.css';
-import Room from "@/app/(afterLogin)/messages/_component/Room";
+import Room from '@/app/(afterLogin)/messages/_component/Room';
+import { Room as IRoom } from '@/model/Room';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  const rooms = session?.user?.email
+    ? await getRooms(session?.user?.email)
+    : [];
   return (
     <main className={style.main}>
       <div className={style.header}>
         <h3>쪽지</h3>
       </div>
-      <Room/>
-      <Room/>
-      <Room/>
-      <Room/>
-      <Room/>
-      <Room/>
+      {rooms.length > 0 &&
+        rooms.map(room => <Room key={room.room} room={room} />)}
     </main>
-  )
+  );
 }
