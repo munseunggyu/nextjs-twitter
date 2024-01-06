9d7dc6ab53578f185a6c38ee66077b8c46c71c24
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-01-06 22:13:25 +0900

    프로필 페이지 메시지 방 이동 버튼 추가

--- src/app/(afterLogin)/[username]/_component/UserInfo.tsx
+++ src/app/(afterLogin)/[username]/_component/UserInfo.tsx
@@ -6,12 +6,14 @@ import { useQuery } from '@tanstack/react-query';
 import { User } from '@/model/User';
 import { getUser } from '@/app/(afterLogin)/[username]/_lib/getUser';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   username: string;
 };
 export default function UserInfo({ username }: Props) {
   const { data: session } = useSession();
+  const router = useRouter();
   const myUserName = session?.user?.email;
   const { data: user, error } = useQuery<
     User,
@@ -24,6 +26,12 @@ export default function UserInfo({ username }: Props) {
     staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
     gcTime: 300 * 1000
   });
+  if (!user) return null;
+  const onMessage = () => {
+    const ids = [session?.user?.email, user.id];
+    ids.sort();
+    router.push(`/messages/${ids.join('-')}`);
+  };
 
   // const followed = !!user?.Followers.find(v => v.id === myUserName);
   if (error) {
@@ -72,9 +80,23 @@ export default function UserInfo({ username }: Props) {
           <div>@{user.id}</div>
         </div>
         {myUserName !== username && (
-          <button className={style.followButton}>
-            {/* {followed ? '팔로잉' : '팔로우'} */}
-          </button>
+          <>
+            <button onClick={onMessage} className={style.messageButton}>
+              <svg
+                viewBox="0 0 24 24"
+                width={18}
+                aria-hidden="true"
+                className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-z80fyv r-19wmn03"
+              >
+                <g>
+                  <path d="M1.998 5.5c0-1.381 1.119-2.5 2.5-2.5h15c1.381 0 2.5 1.119 2.5 2.5v13c0 1.381-1.119 2.5-2.5 2.5h-15c-1.381 0-2.5-1.119-2.5-2.5v-13zm2.5-.5c-.276 0-.5.224-.5.5v2.764l8 3.638 8-3.636V5.5c0-.276-.224-.5-.5-.5h-15zm15.5 5.463l-8 3.636-8-3.638V18.5c0 .276.224.5.5.5h15c.276 0 .5-.224.5-.5v-8.037z"></path>
+                </g>
+              </svg>
+            </button>
+            <button className={style.followButton}>
+              {/* {followed ? '팔로잉' : '팔로우'} */}
+            </button>
+          </>
         )}
       </div>
     </>
